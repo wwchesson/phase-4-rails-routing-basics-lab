1474a4492c3dd6c84d27c9300536a7f4ebdac602
Will <willchesson@gmail.com>
2022-06-29 13:06:29 -0400

    commit tree 60ac66fd0226784d7151a4570d8983d977ff31c9

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,17 @@
+class StudentsController < ApplicationController
+
+    def index
+        students = Student.all
+        render json: students
+    end 
+
+    def grades
+        grades = Student.all.order(grade: :desc)
+        render json: grades
+    end
+
+    def highest_grade
+        student = Student.all.order(grade: :desc).first
+        render json: student
+    end
+end
